120c33a78a8e2eb10c5aeeb5d5c264622505a9a7
JinHwan Kim <spyspysky9@gmail.com>
2017-04-27 11:12:09 +0900

    4/27 jinhwan Qna

---
+++ shoppingmall/src/bookshop/command/QnaDeleteProAction.java
@@ -0,0 +1,5 @@
+package bookshop.command;
+
+public class QnaDeleteProAction {
+
+}
---
+++ shoppingmall/src/bookshop/command/QnaReplyUpdateProAction.java
@@ -0,0 +1,33 @@
+package bookshop.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bookshop.bean.QnaDBBean;
+import bookshop.bean.QnaDataBean;
+
+public class QnaReplyUpdateProAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+
+		int qna_id = Integer.parseInt(request.getParameter("qna_id"));
+		String qna_content = request.getParameter("qna_content");
+
+		// QnA 답변글 수정 정보 설정
+		QnaDataBean qna = new QnaDataBean();
+		qna.setQna_id(qna_id);
+		qna.setQna_content(qna_content);
+
+		// QnA 답변글 수정 처리
+		QnaDBBean qnaProcess = QnaDBBean.getInstance();
+		int check = qnaProcess.updateArticle(qna);
+
+		request.setAttribute("check", new Integer(check));
+
+		return "mngr/qnaProcess/qnaReplyUpdatePro.jsp";
+	}
+
+}
