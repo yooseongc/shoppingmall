6b9bd8e41aa3f6764a024159856052bcb260599f
Lee Kang Hyun <dlrkdsus1@naver.com>
2017-04-28 10:47:55 +0900

    lkh 2017.4.28 10:47

--- shoppingmall/src/bookshop/command/BuyFormAction.java
+++ shoppingmall/src/bookshop/command/BuyFormAction.java
@@ -5,20 +5,46 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bookshop.bean.BuyDBBean;
+import bookshop.bean.CartDBBean;
 import bookshop.bean.CartDataBean;
+import bookshop.bean.LogonDBBean;
+import bookshop.bean.LogonDataBean;
 
-public class BuyFormAction implements CommandAction{
+public class BuyFormAction implements CommandAction {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		request.setCharacterEncoding("utf-8");
-		
-		String buyer = request.getParameter("buyer");
-		
-		List<CartDataBean> cartList = null;
-		List<String> accountLists = null;
 
-		return null;
+		String buyer = request.getParameter("buyer");
+
+		List<CartDataBean> cartLists = null;
+		List<String> accountLists = null;
+		LogonDataBean member = null;
+		int count = 0;
+
+		// 해당 buyer의 장바구니 목록의 수를 얻어냄
+		CartDBBean bookProcess = CartDBBean.getInstance();
+		count = bookProcess.getListCount(buyer);
+
+		if (count > 0) { // 장바구니 목록이 있으면 수행
+			cartLists = bookProcess.getCart(buyer, count);
+			request.setAttribute("cartLists", cartLists);
+		}
+
+		// 구매에 필요한 buyer의 정보를 얻어냄
+		LogonDBBean memberProcess = LogonDBBean.getInstance();
+		member = memberProcess.getMember(buyer);
+
+		// 구매에 필요한 결제 계좌를 얻어냄
+		BuyDBBean buyProcess = BuyDBBean.getInstance();
+		accountLists = buyProcess.getAccount();
+
+		request.setAttribute("member", member);
+		request.setAttribute("accountLists", accountLists);
+		request.setAttribute("type", new Integer(1));
+		return "/buy/buyForm.jsp";
 	}
 
 }
